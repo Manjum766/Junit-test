f398a82a9204e8ef2c695fc09bf99eecf7ddd36f
Manjum766 <134386859+Manjum766@users.noreply.github.com>
2024-06-13 15:05:32 +0530

    Update BuildJobTest.java
    import java.util.concurrent.TimeUnit;1

--- src/test/java/com/demo/qa/test/BuildJobTest.java
+++ src/test/java/com/demo/qa/test/BuildJobTest.java
@@ -1,6 +1,7 @@
 package com.demo.qa.test;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
